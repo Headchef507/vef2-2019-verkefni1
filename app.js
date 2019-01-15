a1bf3212232de4b54d7ca3f7872a51fc20e0497a
Headchef507 <32708897+Headchef507@users.noreply.github.com>
2019-01-15 12:27:10 +0000

    Fyrsti kóðinn í app.js
    HelloWorld

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 /* todo, setja upp express */
+const express = require('express')
+const app = express()
+const port = 3000 //rétt port
+//vantað path
+
+app.get('/', (req,res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
