0c6f98a1d17e2093e3bf86aa30fedcdc4399bbb4
Headchef507 <32708897+Headchef507@users.noreply.github.com>
2019-01-23 21:18:48 +0000

    Whole lot of stuff

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const host = '127.0.0.1';
 const port = 3000;
 
 
-//const path = require('path');
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/img', express.static(path.join(__dirname, 'articles/img')));
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
+
+//app.use('/', img);
 
 
 /*app.use((req, res) => {
--- lectures.js
+++ lectures.js
@@ -7,16 +7,39 @@ const app = express();
 
 const router = express.Router();
 
+const readdirAsync = util.promisify(fs.readdir);
+
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
+async function readLecturesList() {
+  const files = await readdirAsync('/public');
+}
+
 async function list(req, res) {
-  /* todo útfæra */
+  const title = 'Fyrirlestrar';
+  const classes = await readLecturesList();
+
+  const {lectures} = data;
+  res.render('index', {title, lectures});
+  console.log(lectures);
+  
 }
 
 async function lecture(req, res, next) {
-  /* todo útfæra */
+  const { slug } = req.params;
+  const lecutres = await readLecturesList();
+
+  const foundLecture = articles.find(a => a.slug ===slug);
+
+  if(!foundLecture){
+    return next
+  }
+
+  const { title } = foundLecture;
+
+  return res.render('lecture', { title, article: foundLecture });
 }
 
 router.get('/', catchErrors(list));
