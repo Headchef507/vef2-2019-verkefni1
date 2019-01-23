89e5a42acccdce6371eefa2b9a015830760ffc81
Headchef507 <32708897+Headchef507@users.noreply.github.com>
2019-01-23 12:24:41 +0000

    teacher knows best
    tók mikið frá kennaranum

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-/* todo, setja upp express */
-const express = require('express')
-const app = express()
-const port = 3000 //rétt port
-//vantað path
+const express = require('express');
 
-app.get('/', (req,res) => res.send('Hello World!'))
+const app = express();
+const host = '127.0.0.1';
+const port = 3000;
 
-app.listen(port, () => console.log('Example app listening on port ${port}!'))
+
+//const path = require('path');
+
+
+app.use((req, res) => {
+    res.send('Hello World!');
+  });
+  app.listen(port, host, () => {
+    console.log(`Server @ http://${host}:${port}/`);
+  });
